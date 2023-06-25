18aa80dca24cda998683b7d7c77361a25c476475
Ronnie Bunao <ronniebunao001@gmail.com>
2023-06-26 00:39:36 +0800

    Add button that ask for column and rows in grid

--- js.js
+++ js.js
@@ -1,12 +1,17 @@
 const adjustGridSize = document.querySelector('.adjustGridSize');
+const gridContainer = document.querySelector('.gridContainer');
 adjustGridSize.addEventListener('click', () => {
-    const rows = prompt("Please enter the number of rows (e.g., 64)");
-    const columns = prompt("Please enter the number of columns (e.g., 64)");
-    document.querySelector('.gridContainer').style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-    document.querySelector('.gridContainer').style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+    const rowsAndColumns = prompt("Please enter the number of rows and columns (e.g., 'Maximum of 64')");
+    document.querySelector('.gridContainer').style.gridTemplateRows = `repeat(${rowsAndColumns}, 1fr)`;
+    document.querySelector('.gridContainer').style.gridTemplateColumns = `repeat(${rowsAndColumns}, 1fr)`;
 
-    
+    for (let i = 0; i < rowsAndColumns; i++){
+        for (let j = 0; j < rowsAndColumns; j++){
+            const gridItem = document.createElement('div');
+            gridItem.className = gridItem
+            gridContainer.appendChild(gridItem);
+        }
+    }
+
 });
-
-
 
